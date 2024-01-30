475f91558eacfa289c81ed4a1582a5deb13cfc50
alisa <godeater2579@gmail.com>
2024-01-31 02:35:18 +0330

    zoom shortcut

--- .config/fish/conf.d/abbr.fish
+++
@@ -1,5 +0,0 @@
-## main
-abbr vim nvim
-
-## git
-abbr config /usr/bin/git --git-dir=/home/alisa/.dotfiles/ --work-tree=/home/alisa
--- .config/fish/conf.d/startup.fish
+++
@@ -1,2 +0,0 @@
-eval (ssh-agent -c) >/dev/null
-ssh-add ~/.ssh/id_ed25519 >/dev/null 2>&1
